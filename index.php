49c02b960f169050bcfc84254d68c9ee06ab1740
git clone https://github.com/grantwinney/grantwinney.github.io.git <zaya.dodigor@gmail.com>
2016-03-29 11:27:49 -0400

    Project 3 - JavaScript implementation: - Banner slider - Search form validation

--- index.php
+++ index.php
@@ -4,9 +4,103 @@ $current = 'home';
 include ("header.php");
 include ("data.php");
 ?>
+<link href="css/jssor.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="js/jssor.slider.min.js"></script>
 
-<img src="img/banner.jpg" alt="Delkor Banner" width="800" height="280"
-	style="margin: 20 0 20 0;" />
+<script>
+        jssor_1_slider_init = function() {
+            
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:800,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
+            ];
+            
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              },
+              $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $Cols: 1,
+                $Align: 0
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 800);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            //responsive code end
+        };
+    </script>
+
+<div style="margin: 20 0 20 0;">
+	<div id="jssor_1"
+		style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 800px; height: 300px; overflow: hidden; visibility: hidden;">
+		<!-- Loading Screen -->
+		<div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+			<div
+				style="filter: alpha(opacity = 70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+			<div
+				style="position: absolute; display: block; background: url('img/loading.gif') no-repeat center center; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+		</div>
+		<div data-u="slides"
+			style="cursor: default; position: relative; top: 0px; left: 0px; width: 800px; height: 300px; overflow: hidden;">
+			<div data-p="112.50" style="display: none;">
+				<img data-u="image" src="img/banner-1.jpg" />
+			</div>
+			<div data-p="112.50" style="display: none;">
+				<img data-u="image" src="img/banner-2.jpg" />
+			</div>
+			<div data-p="112.50" style="display: none;">
+				<img data-u="image" src="img/banner-3.jpg" />
+			</div>
+			<div data-p="112.50" style="display: none;">
+				<img data-u="image" src="img/banner-4.jpg" />
+			</div>
+		</div>
+		<!-- Bullet Navigator -->
+		<div data-u="navigator" class="jssorb01"
+			style="bottom: 16px; right: 16px;">
+			<div data-u="prototype" style="width: 12px; height: 12px;"></div>
+		</div>
+		<!-- Arrow Navigator -->
+		<span data-u="arrowleft" class="jssora05l"
+			style="top: 0px; left: 8px; width: 40px; height: 40px;"
+			data-autocenter="2"></span> <span data-u="arrowright"
+			class="jssora05r"
+			style="top: 0px; right: 8px; width: 40px; height: 40px;"
+			data-autocenter="2"></span> <a href="http://www.jssor.com"
+			style="display: none">Slideshow Maker</a>
+	</div>
+</div>
+
+<script>
+	jssor_1_slider_init();
+</script>
 
 <h3 style="margin: 0px 20px 10px 0px;">Store Locators</h3>
 <?php
@@ -17,7 +111,7 @@ foreach ( $store_list as $store ) {
 		echo "<div class='img'>";
 	}
 	echo "<a href=store.php?store_id=", urlencode ( $store ['id'] ), ">";
-	echo "<img src='",$store['img'],"' alt='",$store ['name'],"'>";
+	echo "<img src='", $store ['img'], "' alt='", $store ['name'], "'>";
 	echo "</a>";
 	echo "<div class='desc'>", $store ['name'], "</div>";
 	echo "</div>";
--- search.php
+++ search.php
@@ -2,18 +2,22 @@
 $title = 'Search Battery by Car Model';
 $current = 'search';
 include ("header.php");
+include ("data.php");
 
 $current_year = date ( "Y" );
 ?>
 
 <h2>Battery finder</h2>
-<p class="description">Enter your vehicle information and we'll select the right battery for you.</p>
+<p class="description">Enter your vehicle information and we'll select
+	the right battery for you.</p>
 
-<form action="search_result.php" method="get">
+<form action="search_result.php" method="get" name="search_form" onsubmit="return validateForm()">
 	<table border="0px;" style="margin-top: 20">
 		<tr>
 			<td style="padding-right: 25px;">Make</td>
-			<td><select name="make" style="width: 200px">
+			<td style="padding-right: 25px;">
+				<select name="make" style="width: 120px;">
+					<option value=""></option>
 					<option value="Audi">Audi</option>
 					<option value="BMW">BMW</option>
 					<option value="Cadillac">Cadillac</option>
@@ -21,39 +25,89 @@ $current_year = date ( "Y" );
 					<option value="Honda">Honda</option>
 					<option value="Lexus">Lexus</option>
 					<option value="Toyota">Toyota</option>
-			</select></td>
+				</select>
+			</td>
+			<td><p class="validation_error" id="make_error"></p></td>
 		</tr>
 		<tr>
 			<td>Model</td>
-			<td><select name="model" style="width: 200px">
+			<td><select name="model" style="width: 120px">
+					<option value=""></option>
 					<option value="Sedan">Sedan</option>
 					<option value="SUV">SUV</option>
 					<option value="Truck">Truck</option>
 					<option value="Van">Van</option>
 			</select></td>
+			<td><p class="validation_error" id="model_error"></p></td>
 		</tr>
 		<tr>
 			<td>Year</td>
-			<td><select name="year" style="width: 200px">
-				<?php
-				foreach ( range ( $current_year, $current_year - 40 ) as $year ) {
-					echo "<option>$year</option>";
-				}
-				?>
-			</select></td>
+			<td>
+				<select name="year" style="width: 120px">
+					<option value=""></option>
+					<?php
+					foreach ( range ( $current_year, $current_year - 40 ) as $year ) {
+						echo "<option>$year</option>";
+					}
+					?>
+				</select>
+			</td>
+			<td><p class="validation_error" id="year_error"></p></td>
 		</tr>
 		<tr>
 			<td>Engine</td>
-			<td><select name="engine" style="width: 200px">
+			<td><select name="engine" style="width: 120px">
+					<option value=""></option>
 					<option value="2.3-L5(Gas)">2.3 - L5 (Gas)</option>
 					<option value="3.3-V6(Gas)">3.3 - V6 (Gas)</option>
 					<option value="4.7-V8(Gas)">4.7 - V8 (Gas)</option>
 					<option value="5.0-V8(Gas)">5.0 - V8 (Gas)</option>
 			</select></td>
+			<td><p class="validation_error" id="engine_error"></p></td>
 		</tr>
 	</table>
-	<br>
-	<input type="submit" value="Submit">
+	<br> <input type="submit" value="Search" id="search">
 </form>
 
 <?php include("footer.html");?>
+
+<script type="text/javascript">
+function validateForm() {
+    var make = document.forms["search_form"]["make"].value;
+    var model = document.forms["search_form"]["model"].value;
+    var year = document.forms["search_form"]["year"].value;
+    var engine = document.forms["search_form"]["engine"].value;
+
+    var result = true;
+    
+    if (make == null || make == "") {
+        document.getElementById('make_error').innerHTML="Please select a make!";
+        result = false;
+    } else {
+    	document.getElementById('make_error').innerHTML="";
+    }
+
+    if (model == null || model == "") {
+        document.getElementById('model_error').innerHTML="Please select a model!";
+        result = false;
+    } else {
+    	document.getElementById('model_error').innerHTML="";
+    }
+
+    if (year == null || year == "") {
+        document.getElementById('year_error').innerHTML="Please select a year!";
+        result = false;
+    } else {
+    	document.getElementById('year_error').innerHTML="";
+    }
+
+    if (engine == null || engine == "") {
+        document.getElementById('engine_error').innerHTML="Please select a engine!";
+        result = false;
+    } else {
+    	document.getElementById('engine_error').innerHTML="";
+    }
+
+    return result;
+}
+</script>
